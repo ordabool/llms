9aad94380a2c72244406270eea365cdb68bd3a4c
Or Dabool <or.dabool@gmail.com>
2025-08-02 12:35:24 +0300

    Add host and change model

--- agno/src/level_1_agent.py
+++ agno/src/level_1_agent.py
@@ -1,12 +1,17 @@
+import os
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.models.ollama import Ollama
 from agno.tools.yfinance import YFinanceTools
 
 agent = Agent(
-    model=Ollama(id="qwen3:8b"),
+    model=Ollama(
+        id="llama3.2:3b",
+        host=os.getenv("OLLAMA_HOST"),
+        options={"temperature": 0.1},
+    ),
     tools=[YFinanceTools(stock_price=True)],
-    instructions="Fetch stock price using the tool. Use tables to display data. Don't include any other text.",
+    instructions="Fetch stock price using the tool. Display only the price",
     markdown=True,
 )
 
@@ -17,4 +22,4 @@ agent = Agent(
 #     markdown=True,
 #     # debug_mode=True,
 # )
-agent.print_response("What is the stock price of Nvidia?", stream=True)
+agent.print_response("What is the stock price of Apple?", stream=True)
